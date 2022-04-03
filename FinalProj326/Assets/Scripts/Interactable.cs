6e227b3e2a181b008d989dc05419a2b714242489
AwesomenessPrimal <brendanfisher88@gmail.com>
2022-04-03 11:05:46 -0400

    Created scene and Abstract Class

---
+++ FinalProj326/Assets/Scripts/Interactable.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Interactable : MonoBehaviour
+{
+    public string itemName;
+    private bool canInteract;
+
+    void Update()
+    {
+        
+    }
+
+    public abstract void Interact();
+}
