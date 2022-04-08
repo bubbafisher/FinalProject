a84a7264e87223bbd767a70806f2d0839d0123f9
tolivera11 <tianna.olivera@gmail.com>
2022-04-08 00:07:31 -0400

    Option Menu start

---
+++ FinalProj326/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+
+public class SettingsMenu : MonoBehaviour
+
+{
+    public AudioMixer MainAudioMix;
+    public void SetVolume(float volume)
+    {
+        MainAudioMix.SetFloat("volume",volume);
+        MainAudioMix.SetFloat("Volume", Mathf.Log10(volume) * 20);
+
+    }
+
+    public void ToggleFullscreen(bool fullscreen)
+    {
+        
+        Screen.fullScreen = fullscreen;
+        if (Screen.fullScreen)
+        {
+            Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+        }
+        else
+        {
+            Screen.fullScreenMode = FullScreenMode.Windowed;
+        }
+    }
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+}
