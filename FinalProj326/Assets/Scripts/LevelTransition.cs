b27e24b80dc96be2667dceaf929e355310ac404f
AwesomenessPrimal <brendanfisher88@gmail.com>
2022-04-27 15:50:25 -0400

    Added transition to final level

--- FinalProj326/Assets/Scripts/LevelTransition.cs
+++ FinalProj326/Assets/Scripts/LevelTransition.cs
@@ -2,13 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+using StarterAssets;
 
 public class LevelTransition : MonoBehaviour
 {
     public int sceneID;
+    public bool finalLevel = false;
     void OnTriggerEnter(Collider other)
     {
-        PlayerPrefs.SetString("SavedLevel", "level"+sceneID);
-        SceneManager.LoadScene(sceneID);
+        if (!finalLevel)
+        {
+            PlayerPrefs.SetString("SavedLevel", "level" + sceneID);
+            SceneManager.LoadScene(sceneID);
+        }
+        else 
+        {
+            other.gameObject.GetComponent<PlayerInput>().enabled = false;
+            other.gameObject.GetComponent<StarterAssetsInputs>().SetCursorState(false);
+            SceneManager.LoadScene("EndGame");
+        }
+        
     }
 }
